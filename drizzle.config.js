7a82e0b45c24ad442972aacecd5a3473dbd97f45
sandipsawant10 <sandipsawant503@gmail.com>
2026-04-06 17:13:35 +0530

    feat: add drizzle configuration for database connection

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/config/schema.js',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL || process.env.VITE_NEON_CONNECTION_STRING,
+  },
+});
